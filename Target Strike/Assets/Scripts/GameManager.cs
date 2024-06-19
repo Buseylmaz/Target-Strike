4648eaeabb8e49386c6da1ac8c7c1aeb767e4316
Buse Yılmaz <bsylm@outlook.com>
2024-06-19 15:10:08 +0300

    Game Over and Restart

--- Target Strike/Assets/Scripts/GameManager.cs
+++ Target Strike/Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,20 +11,28 @@ public class GameManager : MonoBehaviour
 
     float spawnRate = 1.0f;
 
+
+    [Header("Game Over")]
+    [SerializeField] TextMeshProUGUI gameOverText;
+    [SerializeField] Button restartButton;
+    public bool isGameActive;
+
+    [Header("Score")]
     [SerializeField] TextMeshProUGUI scoreText;
     int score;
 
 
+
     private void Start()
     {
+        isGameActive = true;
         score = 0;
-
         StartCoroutine(SpawnTarget());
     }
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
 
@@ -37,6 +47,18 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
 }
